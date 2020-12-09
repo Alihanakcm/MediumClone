435427d6e4909021d639a9c147682838779f4b2c
Alihan Akçam <alihanakcm@gmail.com>
2020-12-09 20:07:54 +0300

    chore: configurations done in frontend

--- Frontend/blog-side/src/app/app.module.ts
+++ Frontend/blog-side/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { AuthenticationComponent } from './components/authentication/authentication.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomePageComponent,
+    AuthenticationComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
