53125af788927cb867648aa8ccb17e7e72afa262
Alihan Akçam <alihanakcm@gmail.com>
2020-12-09 20:07:21 +0300

    feat: routing added in frontend

--- Frontend/blog-side/src/app/app-routing.module.ts
+++ Frontend/blog-side/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContentComponent } from './components/content/content.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
+  { path: 'content', component: ContentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
