7d34cb641e4dd948412286b195bfa2efc7af3aa7
Alihan Akçam <alihanakcm@gmail.com>
2020-12-09 19:14:07 +0300

    feat: remove like route added in backend

--- Backend/src/api/routes/like.js
+++ Backend/src/api/routes/like.js
@@ -7,4 +7,8 @@ app.post("/like", authToken.authenticateToken, (request, response) => {
     services.likeService.likePost(request.query.postId, request.query.userId).then(res => response.send(res)).catch(err => console.log(err));
 });
 
+app.delete("/like", authToken.authenticateToken, (request, response) => {
+    services.likeService.removeLike(request.query.id).then(res => response.send(res)).catch(err => console.log(err))
+})
+
 module.exports = app;
--- Backend/src/services/like-service.js
+++ Backend/src/services/like-service.js
@@ -3,15 +3,23 @@ var db = require("../database/index");
 likeService = {
     likePost(postId, userId) {
         return new Promise((resolve, reject) => {
-            db.connection.query(`INSERT INTO likes (post_id,user_id) VALUES (${postId},${userId}) `, (err, result, fields) => {
-                if (err) throw err;
-                const error = false;
+            db.connection.query(`INSERT INTO likes (post_id,user_id) VALUES (${postId},${userId}) `, (error, result, fields) => {
                 if (!error)
                     resolve(200);
                 else
-                    reject(error);
+                    reject(error.sqlMessage);
             });
 
+        });
+    },
+    removeLike(id) {
+        return new Promise((resolve, reject) => {
+            db.connection.query(`DELETE FROM likes WHERE id=${id}`, (error, result, fields) => {
+                if (!error)
+                    resolve(200);
+                else
+                    reject(error.sqlMessage);
+            });
         });
     }
 }
