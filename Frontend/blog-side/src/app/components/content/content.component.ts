286a5f6f734767fa70d1d57c947a010c478bffe8
Alihan Akçam <alihanakcm@gmail.com>
2020-12-17 23:29:41 +0300

    feat: content design added in frontend

--- Frontend/blog-side/src/app/components/content/content.component.ts
+++ Frontend/blog-side/src/app/components/content/content.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Post } from 'src/app/entities/post';
+import { PostService } from 'src/app/services/post-service/post.service';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
+  styleUrls: ['./content.component.css'],
+  providers: [PostService]
 })
 export class ContentComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private postService: PostService, private activatedRoute: ActivatedRoute) { }
+  singlePost: Post;
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      this.postService.getsinglePost(params["postId"]).subscribe(data => {
+        this.singlePost = data;
+      });
+    });
   }
-
 }
