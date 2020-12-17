1b349a8dc6ad7c3103fe8ea690897054e370e5a9
Alihan Akçam <alihanakcm@gmail.com>
2020-12-17 23:29:25 +0300

    feat home page component design added

--- Frontend/blog-side/src/app/components/home-page/home-page.component.ts
+++ Frontend/blog-side/src/app/components/home-page/home-page.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import {PostService} from '../../services/post-service/post.service';
+import { Post } from 'src/app/entities/post';
+import { PostService } from '../../services/post-service/post.service';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css'],
-  providers:[PostService]
+  providers: [PostService]
 })
 export class HomePageComponent implements OnInit {
 
-  constructor(private postService:PostService) { }
-
+  constructor(private postService: PostService) { }
+  allPosts: Post[];
   ngOnInit(): void {
-    // this.postService.getPosts();
+    this.postService.getPosts().subscribe(data => {
+      this.allPosts = data;
+    });
   }
 
 }
