475b746f7e5dfbae351e12ef38be1ebaa522e86e
Alihan Akçam <alihanakcm@gmail.com>
2020-12-18 18:27:14 +0300

    fix: some neccessery arrengmanet done

--- Frontend/blog-side/src/app/components/navigation/navigation.component.ts
+++ Frontend/blog-side/src/app/components/navigation/navigation.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from '../../services/user-service/user.service';
-import { UserLogin } from '../../entities/userLogin';
-import { User } from '../../entities/user';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user-service/user.service';
+
 
 @Component({
   selector: 'app-navigation',
@@ -12,52 +11,16 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor(private userService: UserService, private formBuilder: FormBuilder) { }
+  constructor(private userService: UserService,private router:Router) { }
 
-  isLogin = true;
-  error: string;
-  success: string;
-  registerForm: FormGroup;
-  loginForm: FormGroup;
-  user: User = new User();
-  userLogin: UserLogin = new UserLogin();
-
-  createRegisterForm() {
-    this.registerForm = this.formBuilder.group({
-      fullName: ["", Validators.required],
-      username: ["", Validators.required],
-      email: ["", Validators.required],
-      password: ["", Validators.required]
-    });
-  }
-
-  createLoginForm() {
-    this.loginForm = this.formBuilder.group({
-      email: ["", Validators.required],
-      password: ["", Validators.required]
-    });
-  }
   ngOnInit(): void {
-    this.createRegisterForm();
-    this.createLoginForm();
   }
-  register() {
-    if (this.registerForm.valid) {
-      this.user = Object.assign({}, this.registerForm.value);
-      this.userService.register(this.user).subscribe(res => {
-        if (res["res"] != 200)
-          this.error = res["res"];
-        else
-          this.success = "Kaydınız başarıyla yapılmıştır";
-      });
-    }
+
+  get isAuthenticated() {
+    return this.userService.loggedIn();
   }
-  asd() {
-    if (this.loginForm.valid) {
-      this.userLogin = Object.assign({}, this.loginForm.value);
-      this.userService.login(this.userLogin).subscribe(res => {
-        console.log(res);
-      });
-    }
+  logOut() {
+    this.userService.logOut();
+    this.router.navigateByUrl('/home');
   }
 }
