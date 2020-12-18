7d5aec15607a75a6bb4ffe33a070b7f36c4a126f
Alihan Akçam <alihanakcm@gmail.com>
2020-12-18 14:37:00 +0300

    feat: get single post routing added in backend

--- Backend/src/api/routes/post.js
+++ Backend/src/api/routes/post.js
@@ -4,12 +4,16 @@ const app = express();
 const services = require("../../services/index");
 var authToken = require("../middlewares/authentication/index");
 
-app.get("/posts", authToken.authenticateToken, (request, response) => {
+app.get("/posts", (request, response) => {
     services.postService.getPosts().then(data => response.send(data)).catch(err => console.log(err));
 });
 
+app.get("/post/:id", (request, response) => {
+    services.postService.getPost(request.params.id).then(data => response.send(data)).catch(err => console.log(err));
+});
+
 app.post("/new-post", authToken.authenticateToken, (request, response) => {
-    services.postService.createPost(request.query.userId,request.body).then(res => response.send(res)).catch(err => console.log(err));
+    services.postService.createPost(request.query.userId, request.body).then(res => response.send(res)).catch(err => console.log(err));
 });
 
 module.exports = app;
