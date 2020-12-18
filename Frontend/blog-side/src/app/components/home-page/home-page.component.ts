d50658889021c8fe660e73b62aaff731588afa2c
Alihan Akçam <alihanakcm@gmail.com>
2020-12-18 18:25:48 +0300

    feat: is login control added

--- Frontend/blog-side/src/app/components/home-page/home-page.component.ts
+++ Frontend/blog-side/src/app/components/home-page/home-page.component.ts
@@ -1,20 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Post } from 'src/app/entities/post';
+import { UserService } from 'src/app/services/user-service/user.service';
 import { PostService } from '../../services/post-service/post.service';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css'],
-  providers: [PostService]
+  providers: [PostService, UserService]
 })
 export class HomePageComponent implements OnInit {
 
-  constructor(private postService: PostService) { }
+  constructor(private postService: PostService, private userService: UserService, private router: Router) { }
   allPosts: Post[];
   ngOnInit(): void {
     this.postService.getPosts().subscribe(data => {
       this.allPosts = data;
     });
   }
+  get isAuthenticated() {
+    return this.userService.loggedIn();
+  }
+  readMore(id: number) {
+    console.log("isLog" + this.isAuthenticated);
 
+    if (!this.isAuthenticated)
+      this.router.navigateByUrl('/post/' + id);
+    else
+      this.router.navigateByUrl("/login-register");
+  }
 }
