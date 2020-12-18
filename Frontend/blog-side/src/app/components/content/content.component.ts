716bf6cdc6ff483ac77f03c68ebcdbccfeee0b02
Alihan Akçam <alihanakcm@gmail.com>
2020-12-18 23:48:27 +0300

    feat: send comment form added in frontend

--- Frontend/blog-side/src/app/components/content/content.component.ts
+++ Frontend/blog-side/src/app/components/content/content.component.ts
@@ -1,23 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Post } from 'src/app/entities/post';
 import { PostService } from 'src/app/services/post-service/post.service';
+import { CommentService } from 'src/app/services/comment-service/comment.service';
+
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css'],
-  providers: [PostService]
+  providers: [PostService, CommentService]
 })
 export class ContentComponent implements OnInit {
 
-  constructor(private postService: PostService, private activatedRoute: ActivatedRoute) { }
+  constructor(private postService: PostService, private commentService: CommentService, private activatedRoute: ActivatedRoute, private formBuilder: FormBuilder) { }
   singlePost: Post;
+  commentForm: FormGroup;
+  comment = new Comment();
+  success:string;
   ngOnInit(): void {
+    this.createCommentForm();
     this.activatedRoute.params.subscribe(params => {
       this.postService.getsinglePost(params["postId"]).subscribe(data => {
         this.singlePost = data;
+        console.log(this.singlePost[0].id)
+
       });
     });
+  }
+
+  createCommentForm() {
+    this.commentForm = this.formBuilder.group({
+      comment: ["", Validators.required],
+      name: ["", Validators.required],
+    })
+  }
+  sendComment() {
+    if (this.commentForm.valid) {
+      this.comment = Object.assign({}, this.commentForm.value);
+      this.commentService.newComment(this.singlePost[0].userId, this.singlePost[0].id, this.comment).subscribe(res => {
+        if (res["res"] == 200)
+          this.success = "Yorumunuz başarılı bir şekilde kaydedildi";
+      });
+    }
   }
 }
--- Frontend/blog-side/src/app/services/comment-service/comment.service.ts
+++ Frontend/blog-side/src/app/services/comment-service/comment.service.ts
@@ -1,10 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { catchError } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
 @Injectable()
 export class CommentService {
 
-  constructor(private http:HttpClient) { }
-
-  newComment(userId,postId,comment){}
+  constructor(private http: HttpClient) { }
+  TOKEN_KEY = "token";
+  newComment(userId, postId, comment): Observable<Response> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'authorization': 'Bearer ' + localStorage.getItem(this.TOKEN_KEY)
+      })
+    };
+    return this.http.post<Response>(environment.url + "/comment?userId=" + userId + "&postId=" + postId, comment, httpOptions).pipe(catchError(this.handleError));
+  }
+  handleError(err: HttpErrorResponse) {
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = 'Something went wrong' + err.message;
+    } else {
+      errorMessage = 'System-something went wrong ' + err.message;
+    }
+    return throwError(errorMessage);
+  }
 }
