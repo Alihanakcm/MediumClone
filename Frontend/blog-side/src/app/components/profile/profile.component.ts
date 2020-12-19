a00957aae8c282d238209b1e4ddd23173c05d84f
Alihan Akçam <alihanakcm@gmail.com>
2020-12-19 23:00:17 +0300

    feat: profile component operations added

--- Frontend/blog-side/src/app/components/profile/profile.component.ts
+++ Frontend/blog-side/src/app/components/profile/profile.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/entities/user';
+import { UserService } from 'src/app/services/user-service/user.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
+  providers: [UserService]
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private userService: UserService) { }
+  user: User;
   ngOnInit(): void {
+    this.userService.getCurrentUser().subscribe(data => { this.user = data[0] });
   }
 
 }
