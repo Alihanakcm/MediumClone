1840ded490651ef01dd97c98f030cc0f9cd02530
Alihan Akçam <alihanakcm@gmail.com>
2020-12-19 22:59:38 +0300

    feat: profile component routing added in frontend

--- Frontend/blog-side/src/app/app-routing.module.ts
+++ Frontend/blog-side/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { ContentComponent } from './components/content/content.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { LoginRegisterComponent } from './components/login-register/login-register.component';
 import { NewPostComponent } from './components/new-post/new-post.component'
+import { ProfileComponent } from './components/profile/profile.component';
 
 
 const routes: Routes = [
@@ -11,7 +12,8 @@ const routes: Routes = [
   { path: 'home', component: HomePageComponent },
   { path: 'post/:postId', component: ContentComponent },
   { path: 'login-register', component: LoginRegisterComponent },
-  { path: 'new-post', component: NewPostComponent }
+  { path: 'new-post', component: NewPostComponent },
+  { path: 'profile', component: ProfileComponent }
 ];
 
 @NgModule({
