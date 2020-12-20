eae4842493ae129befa8a8a85178e1a30e32be4e
Alihan Akçam <alihanakcm@gmail.com>
2020-12-20 20:39:36 +0300

    feat: like operaitons added in frontend

--- Frontend/blog-side/src/app/components/content/content.component.ts
+++ Frontend/blog-side/src/app/components/content/content.component.ts
@@ -1,40 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Post } from 'src/app/entities/post';
 import { PostService } from 'src/app/services/post-service/post.service';
 import { CommentService } from 'src/app/services/comment-service/comment.service';
 import { UserService } from 'src/app/services/user-service/user.service';
-
-
+import { LikeService } from 'src/app/services/like-service/like.service';
+import { User } from 'src/app/entities/user';
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css'],
-  providers: [PostService, CommentService]
+  providers: [PostService, CommentService, LikeService],
+  encapsulation: ViewEncapsulation.None
 })
 export class ContentComponent implements OnInit {
 
-  constructor(private postService: PostService, private userService: UserService, private commentService: CommentService, private activatedRoute: ActivatedRoute, private formBuilder: FormBuilder, private router: Router) { }
+  constructor(private postService: PostService, private likeService: LikeService, private userService: UserService, private commentService: CommentService, private activatedRoute: ActivatedRoute, private formBuilder: FormBuilder, private router: Router) { }
   singlePost: Post;
   commentForm: FormGroup;
   comment = new Comment();
   success: string;
   comments: Comment[];
-  user: number;
-
+  user: User;
+  postId: number;
+  liked: boolean = false;
+  likeCount: number;
 
   ngOnInit(): void {
-    this.userService.getCurrentUser().subscribe(data => { this.user = data[0] });
+        document.body.className = "bg";
+    this.userService.getCurrentUser().subscribe(data => {
+      this.user = data[0];
+    });
     this.createCommentForm();
     this.activatedRoute.params.subscribe(params => {
+      this.postId = params["postId"];
       this.postService.getsinglePost(params["postId"]).subscribe(data => {
         this.singlePost = data;
+        this.likeCount = data[0].likeCount;
       });
       this.commentService.getComments(params["postId"]).subscribe(data => {
-        this.comments = data
+        this.comments = data;
+      });
+      this.likeService.isLiked(this.postId, this.userService.getUserName()).subscribe(data => {
+        this.liked = data["res"];
       });
     });
+  }
+  ngOnDestroy(): void {
+    document.body.className = "";
   }
   createCommentForm() {
     this.commentForm = this.formBuilder.group({
@@ -47,7 +61,7 @@ export class ContentComponent implements OnInit {
       this.comment = Object.assign({}, this.commentForm.value);
       this.commentService.newComment(this.user["id"], this.singlePost[0].id, this.comment).subscribe(res => {
         if (res["res"] == 200) {
-          this.singlePost[0].commentCount+=1;
+          this.singlePost[0].commentCount += 1;
           this.success = "Yorumunuz başarılı bir şekilde kaydedildi";
           this.comment["username"] = this.user["username"];
           this.comments.push(this.comment);
@@ -55,5 +69,15 @@ export class ContentComponent implements OnInit {
       });
     }
   }
-
+  like(isLike: boolean, postId: number, userId: number) {
+    this.liked = isLike;
+    if (isLike) {
+      this.likeCount += 1;
+      this.likeService.likePost(postId, this.user.username).subscribe();
+    }
+    else {
+      this.likeCount -= 1;
+      this.likeService.removeLike(postId, this.user.username).subscribe();
+    }
+  }
 }
--- Frontend/blog-side/src/app/services/like-service/like.service.ts
+++ Frontend/blog-side/src/app/services/like-service/like.service.ts
@@ -1,10 +1,47 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { catchError, tap } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
 @Injectable()
 export class LikeService {
 
   constructor(private http: HttpClient) { }
-  likePost(postId, userId) { }
-  removeLike(likeId) { }
+  TOKEN_KEY = "token";
+  likePost(postId, username): Observable<Response> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'authorization': 'Bearer ' + localStorage.getItem(this.TOKEN_KEY)
+      })
+    };
+    return this.http.post<Response>(environment.url + "/like?postId=" + postId + "&username=" + username, {}, httpOptions).pipe(catchError(this.handleError));
+  }
+  removeLike(postId, username): Observable<Response> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'authorization': 'Bearer ' + localStorage.getItem(this.TOKEN_KEY)
+      })
+    };
+    return this.http.delete<Response>(environment.url + "/like?postId=" + postId + "&username=" + username, httpOptions).pipe(catchError(this.handleError))
+  }
+  isLiked(postId, username): Observable<boolean> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'authorization': 'Bearer ' + localStorage.getItem(this.TOKEN_KEY)
+      })
+    };
+    return this.http.get<boolean>(environment.url + "/like?postId=" + postId + "&username=" + username, httpOptions).pipe(catchError(this.handleError))
+  }
+  handleError(err: HttpErrorResponse) {
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = 'Something went wrong' + err.message;
+    } else {
+      errorMessage = 'System-something went wrong ' + err.message;
+    }
+    return throwError(errorMessage);
+  }
 }
