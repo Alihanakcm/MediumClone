64d62075ef4868bf96bac615c65fc5a5cffc4ea0
Alihan Akçam <alihanakcm@gmail.com>
2020-12-20 15:10:28 +0300

    feat: get comments route and comment count increase added in backend

--- Backend/src/api/routes/comment.js
+++ Backend/src/api/routes/comment.js
@@ -1,10 +1,15 @@
 var express = require("express");
-const { comment } = require(".");
+
 var app = express();
 var services = require("../../services/index");
 var authToken = require("../middlewares/authentication/index");
 app.post("/comment", authToken.authenticateToken, (request, response) => {
-    services.commentService.postComment(request.query.postId, request.query.userId, request.body).then(res => response.send({res})).catch(err => console.log(err));
+    services.commentService.postComment(request.query.postId, request.query.userId, request.body).then(res => response.send({
+        res
+    })).catch(err => console.log(err));
 });
+app.get("/comments", authToken.authenticateToken, (request, response) => {
+    services.commentService.getComments(request.query.postId).then(res => response.send(res)).catch(err => console.log(err));
+})
 
 module.exports = app;
--- Backend/src/services/comment-service.js
+++ Backend/src/services/comment-service.js
@@ -2,12 +2,32 @@ var db = require("../database/index");
 
 commentService = {
     postComment(postId, userId, comment) {
-        console.log(comment.name);
-        console.log(comment.comment);
         return new Promise((resolve, reject) => {
             db.connection.query(`INSERT INTO comments (name,comment,postId,userId) VALUES ("${comment.name}","${comment.comment}",${postId},${userId})`, (error, result, fields) => {
-                if (!error)
+                if (!error) {
+                    this.commentCountIncrease(postId);
                     resolve(200);
+                } else
+                    reject(error.sqlMessage);
+            })
+        });
+    },
+    getComments(postId) {
+        return new Promise((resolve, reject) => {
+            db.connection.query(`SELECT c.id,c.name,c.comment,c.postId,c.commentDate,u.username FROM comments c INNER JOIN users u ON c.userId=u.id WHERE postId=${postId}`, (error, result, fields) => {
+                if (!error)
+                    resolve(result);
+                else
+                    reject(error.sqlMessage);
+            })
+        });
+    },
+    commentCountIncrease(postId) {
+        return new Promise((resolve, reject) => {
+            db.connection.query(`UPDATE posts set commentCount=commentCount+1 WHERE id=${postId}
+        `, (error, result, fields) => {
+                if (!error)
+                    resolve(result);
                 else
                     reject(error.sqlMessage);
             })
@@ -15,4 +35,4 @@ commentService = {
     }
 }
 
-module.exports=commentService;
+module.exports = commentService;
